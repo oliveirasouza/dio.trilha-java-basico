f5689d15c4e014ff90fda3405f275cf78a3abb77
oliveirasouza <oliveirasouza00@gmail.com>
2024-06-12 10:03:19 +0100

    operadores unarios aula 4

---
+++ estudosVariaveis/src/main/java/org/example/OperadoresUnarios.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class OperadoresUnarios {
+
+    public static void main(String [] args){
+
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        numero = numero *-1;
+
+        System.out.println(numero);
+
+
+        boolean variavel = true;
+
+        System.out.println(variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
+}
