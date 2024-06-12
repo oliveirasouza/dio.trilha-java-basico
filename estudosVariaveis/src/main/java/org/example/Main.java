c82d26dff7a413b2bae7db6756836fe6b6234500
oliveirasouza <oliveirasouza00@gmail.com>
2024-06-12 09:36:33 +0100

    alterações operadores

--- estudosVariaveis/src/main/java/org/example/Main.java
+++ estudosVariaveis/src/main/java/org/example/Main.java
@@ -12,6 +12,19 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+
+
+
+            final String NOME = "Francisco";
+            System.out.println(NOME);
+
+
+
+
+
+
+
         }
     }
 }
+
---
+++ estudosVariaveis/src/main/java/org/example/Operadores.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Operadores {
+
+    public static void main(String[] args){
+
+    String concatenacao = "?";
+
+    concatenacao = 1+1+1 + " Soma dois  " + 2;
+
+    System.out.println(concatenacao);
+
+    concatenacao = 1+1+1 + " Soma  quatro " + 4;
+
+    System.out.println(concatenacao);
+
+    }
+}
